cf779375646509b69d425db464062f24a7b652e7
FranPerezSevilla <fjperezga@gmail.com>
2026-07-13 16:55:46 +0200

    Connect feedback form to Google Sheets endpoint

--- js/feedback-config.js
+++ js/feedback-config.js
@@ -1,6 +1,5 @@
 // Playtest feedback configuration.
-// Paste your Google Apps Script Web App URL here when ready.
-// Example: window.VD_FEEDBACK_ENDPOINT = "https://script.google.com/macros/s/AKfycb.../exec";
+// Google Apps Script Web App endpoint for external feedback storage.
 
 window.VD_BUILD_VERSION = "prototype-feedback-v1";
-window.VD_FEEDBACK_ENDPOINT = "";
+window.VD_FEEDBACK_ENDPOINT = "https://script.google.com/macros/s/AKfycbx2bwgRkz2kA1l19wOnLQ-A0nFuVYC3gQhHssH79NYMFLpNWOD4kEXMREhFp5s9mNJ44g/exec";
