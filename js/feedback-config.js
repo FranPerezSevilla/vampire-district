864f1ffa3f5163c1ede199479aa73dfc5bbe04d0
FranPerezSevilla <fjperezga@gmail.com>
2026-07-13 16:29:18 +0200

    Add playtest feedback config

---
+++ js/feedback-config.js
@@ -0,0 +1,6 @@
+// Playtest feedback configuration.
+// Paste your Google Apps Script Web App URL here when ready.
+// Example: window.VD_FEEDBACK_ENDPOINT = "https://script.google.com/macros/s/AKfycb.../exec";
+
+window.VD_BUILD_VERSION = "prototype-feedback-v1";
+window.VD_FEEDBACK_ENDPOINT = "";
