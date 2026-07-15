ae03fbf77d203a1c030d2129751c7c386af5b037
FranPerezSevilla <fjperezga@gmail.com>
2026-07-15 16:14:19 +0200

    Render Phaser canvas natively without pixel scaling

--- phaser/src/main.js
+++ phaser/src/main.js
@@ -3,14 +3,23 @@ import { GameScene } from "./scenes/GameScene.js";
 import { UIScene } from "./scenes/UIScene.js";
 import { WORLD } from "./data/balance.js";
 
+const deviceResolution = Math.min(window.devicePixelRatio || 1, 2);
+
 const config = {
   type: Phaser.AUTO,
   parent: "game-root",
   width: WORLD.width,
   height: WORLD.height,
+  resolution: deviceResolution,
   backgroundColor: "#05060b",
-  pixelArt: true,
-  roundPixels: true,
+  pixelArt: false,
+  roundPixels: false,
+  render: {
+    antialias: true,
+    antialiasGL: true,
+    pixelArt: false,
+    roundPixels: false
+  },
   physics: {
     default: "arcade",
     arcade: {
@@ -18,7 +27,7 @@ const config = {
     }
   },
   scale: {
-    mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale.NONE,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
   scene: [BootScene, GameScene, UIScene]
