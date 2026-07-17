257808de34873cacbcf98d4beeaa3295dec10fd5
FranPerezSevilla <fjperezga@gmail.com>
2026-07-17 06:33:42 +0200

    Remove text from objective arrow

--- phaser/src/objective-marker.js
+++ phaser/src/objective-marker.js
@@ -24,23 +24,14 @@ function liveNpc(scene, id) {
   return scene.npcSystem?.npcs?.find(npc => npc.id === id) || null;
 }
 
-function objectivePoint(x, y, layer, label, color) {
-  return { x, y, layer, label, color };
+function objectivePoint(x, y, layer, color) {
+  return { x, y, layer, color };
 }
 
 class PlayerObjectiveArrow {
   constructor(scene) {
     this.scene = scene;
     this.graphics = scene.add.graphics().setDepth(86);
-    this.label = scene.add.text(0, 0, "", {
-      fontFamily: "Arial, Helvetica, sans-serif",
-      fontSize: "11px",
-      fontStyle: "bold",
-      color: "#fff6dc",
-      align: "center"
-    }).setOrigin(0.5, 1).setDepth(87);
-    this.label.setResolution?.(3);
-    this.label.setStroke?.("#05060b", 4);
 
     scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
     scene.events.once(Phaser.Scenes.Events.SHUTDOWN, this.destroy, this);
@@ -49,7 +40,6 @@ class PlayerObjectiveArrow {
   destroy() {
     this.scene.events.off(Phaser.Scenes.Events.UPDATE, this.update, this);
     this.graphics?.destroy();
-    this.label?.destroy();
   }
 
   currentObjective() {
@@ -64,21 +54,15 @@ class PlayerObjectiveArrow {
     if (director.busy || HIDDEN_TUTORIAL_STATES.has(director.state)) return null;
 
     if ((mission.rooftopJumps || 0) < 1) {
-      return objectivePoint(
-        FIRST_JUMP.ax,
-        FIRST_JUMP.ay,
-        FIRST_JUMP.aLayer,
-        "FIRST JUMP",
-        ROUTE_COLOR
-      );
+      return objectivePoint(FIRST_JUMP.ax, FIRST_JUMP.ay, FIRST_JUMP.aLayer, ROUTE_COLOR);
     }
 
     const thug = liveNpc(this.scene, "rooftop_thug");
     if (thug && !thug.dead) {
-      return objectivePoint(thug.x, thug.y, thug.layer, "ROOFTOP BLOCKER", DANGER_COLOR);
+      return objectivePoint(thug.x, thug.y, thug.layer, DANGER_COLOR);
     }
 
-    return objectivePoint(775, 150, LAYERS.ROOF_LOW, "POLICE TIP", TIP_COLOR);
+    return objectivePoint(775, 150, LAYERS.ROOF_LOW, TIP_COLOR);
   }
 
   update(time) {
@@ -159,21 +143,10 @@ class PlayerObjectiveArrow {
 
     this.graphics.lineStyle(2, color, 0.42 + pulse * 0.22)
       .strokeCircle(player.x, player.y, 13 + pulse * 2);
-
-    const labelDistance = Math.min(endDistance * 0.62, 48);
-    const labelX = player.x + ux * labelDistance + px * 13;
-    const labelY = player.y + uy * labelDistance + py * 13;
-
-    this.label
-      .setText(objective.label)
-      .setColor(`#${color.toString(16).padStart(6, "0")}`)
-      .setPosition(labelX, labelY)
-      .setVisible(true);
   }
 
   hide() {
     this.graphics.clear();
-    this.label.setVisible(false);
   }
 }
 
