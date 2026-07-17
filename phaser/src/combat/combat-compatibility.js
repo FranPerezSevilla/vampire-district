8a11f74ec2064cf2d592bd94f171b597dce5621b
FranPerezSevilla <fjperezga@gmail.com>
2026-07-17 16:16:39 +0200

    Avoid duplicate witness escalation from one unarmed hit

--- phaser/src/combat/combat-compatibility.js
+++ phaser/src/combat/combat-compatibility.js
@@ -1,5 +1,9 @@
+import { CombatSystem } from "./CombatSystem.js";
 import { COMBAT_STATES } from "../data/combat.js";
+import { NPC_TYPES } from "../data/npcs.js";
+import { resolveAction } from "../systems/ActionSystem.js";
 import { NpcSystem } from "../systems/NpcSystem.js";
+import { RawAudio } from "../systems/RawAudioSystem.js";
 
 function installDownedMarkerCompatibility() {
   if (NpcSystem.prototype.__nbdDownedMarkerCompatibilityPatch) return;
@@ -18,4 +22,38 @@ function installDownedMarkerCompatibility() {
   NpcSystem.prototype.__nbdDownedMarkerCompatibilityPatch = true;
 }
 
+function installSingleViolenceDispatch() {
+  if (CombatSystem.prototype.__nbdSingleViolenceDispatchPatch) return;
+
+  CombatSystem.prototype.notifyViolence = function notifyViolenceOnce(npc, downed) {
+    resolveAction(this.scene, "stun", {
+      x: npc.x,
+      y: npc.y,
+      layer: npc.layer,
+      target: npc,
+      exclude: [npc],
+      cooldownKey: `unarmed:${this.attack?.serial || this.attackSerial}`,
+      cooldown: 0.05
+    });
+
+    if (npc.type === NPC_TYPES.POLICE) {
+      const reason = `A police officer was ${downed ? "knocked down" : "assaulted"}.`;
+      this.scene.exposureSystem?.forceLevel?.(1, reason);
+      this.scene.policeSystem?.addHeat?.(npc.x, npc.y, downed ? 24 : 15, reason);
+      const zone = this.scene.policeSystem?.zoneAt?.(this.scene.player.x, this.scene.player.y);
+      if (this.scene.policeSystem) {
+        this.scene.policeSystem.lastKnownPlayer = {
+          x: this.scene.player.x,
+          y: this.scene.player.y,
+          zoneId: zone?.id || "district"
+        };
+      }
+      RawAudio.play("police", { cooldown: 0.3 });
+    }
+  };
+
+  CombatSystem.prototype.__nbdSingleViolenceDispatchPatch = true;
+}
+
 installDownedMarkerCompatibility();
+installSingleViolenceDispatch();
