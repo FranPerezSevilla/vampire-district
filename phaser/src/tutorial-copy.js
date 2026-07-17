9d69152eb23370ae6a7fb05e80628417251a669e
FranPerezSevilla <fjperezga@gmail.com>
2026-07-17 16:10:23 +0200

    Load the Milestone 2 combat tutorial copy adapter

--- phaser/src/tutorial-copy.js
+++ phaser/src/tutorial-copy.js
@@ -2,5 +2,6 @@ import "./english-copy.js";
 import "./tutorial-stop-after-tip.js";
 import "./dialogue-layout.js";
 import "./tutorial-encounter-order.js";
+import "./combat/combat-tutorial-copy.js";
 import "./police-informant.js";
 import "./final-report-sire.js";
