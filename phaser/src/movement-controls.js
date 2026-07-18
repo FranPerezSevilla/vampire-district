0355e519a2533d3233b3e99259fe80f314f25166
FranPerezSevilla <fjperezga@gmail.com>
2026-07-18 16:47:54 +0200

    Load Milestone 8 prioritized AI runtime

--- phaser/src/movement-controls.js
+++ phaser/src/movement-controls.js
@@ -5,6 +5,7 @@ import "./input/input-runtime.js";
 import "./input/tutorial-input-adapter.js";
 import "./movement/milestone5-runtime.js";
 import "./world/milestone6-runtime.js";
+import "./ai/milestone8-runtime.js";
 import "./weapons/milestone7-ui.js";
 import { UIScene } from "./scenes/UIScene.js";
 
