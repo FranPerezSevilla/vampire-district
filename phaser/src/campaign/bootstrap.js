f2cdaa4f41bdc9cb3132c65166d67d59728e6d09
FranPerezSevilla <fjperezga@gmail.com>
2026-07-20 08:57:25 +0200

    Release the intro modal only after restored UI is ready

--- phaser/src/campaign/bootstrap.js
+++ phaser/src/campaign/bootstrap.js
@@ -2,6 +2,16 @@ import { installCampaignBrowserApi } from "./CampaignBrowserApi.js";
 import { CampaignCheckpointSystem } from "./CampaignCheckpointSystem.js";
 import { campaign } from "./preload.js";
 
+function releaseRestoredIntro(game, checkpoints) {
+  if (!checkpoints.restored) return;
+  const uiScene = game.scene?.getScene?.("UIScene");
+  if (!uiScene?.dom) {
+    window.requestAnimationFrame(() => releaseRestoredIntro(game, checkpoints));
+    return;
+  }
+  if (uiScene.introOpen) uiScene.closeIntro?.();
+}
+
 function attachCampaignRuntime() {
   const game = window.NBD_PHASER_GAME;
   const scene = game?.scene?.getScene?.("GameScene");
@@ -22,9 +32,7 @@ function attachCampaignRuntime() {
   const updateCheckpoint = () => checkpoints.update();
   scene.events.on(Phaser.Scenes.Events.POST_UPDATE, updateCheckpoint);
   const uninstallApi = installCampaignBrowserApi(scene, campaign, checkpoints);
-
-  const uiScene = game.scene?.getScene?.("UIScene");
-  if (checkpoints.restored && uiScene?.introOpen) uiScene.closeIntro?.();
+  releaseRestoredIntro(game, checkpoints);
 
   scene.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
     scene.events.off(Phaser.Scenes.Events.POST_UPDATE, updateCheckpoint);
