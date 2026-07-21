dbaa67ddee38ea403e58c56623579165e9675b43
FranPerezSevilla <fjperezga@gmail.com>
2026-07-21 13:47:26 +0200

    Increase vehicle speed and tune handbrakes

--- phaser/src/data/vehicles.js
+++ phaser/src/data/vehicles.js
@@ -3,10 +3,10 @@ import { LAYERS } from "./district.js";
 export const VEHICLE_OWNERSHIP = Object.freeze({ PARKED: "parked", OWNED: "owned", STOLEN: "stolen", FACTION: "faction", POLICE: "police" });
 
 export const VEHICLE_ARCHETYPES = Object.freeze({
-  compact: Object.freeze({ id: "compact", label: "Compact", width: 28, height: 14, maxSpeed: 188, reverseSpeed: 66, acceleration: 122, reverseAcceleration: 78, brake: 214, drag: 54, steerRate: 2.55, maxHealth: 72, trunkCapacity: 2, cameraZoomFactor: 0.78, color: 0x78c7a3, trim: 0xd7ffec }),
-  sedan: Object.freeze({ id: "sedan", label: "Sedan", width: 34, height: 16, maxSpeed: 210, reverseSpeed: 72, acceleration: 112, reverseAcceleration: 74, brake: 202, drag: 48, steerRate: 2.30, maxHealth: 88, trunkCapacity: 4, cameraZoomFactor: 0.74, color: 0x9a7ab8, trim: 0xefe6ff }),
-  van: Object.freeze({ id: "van", label: "Van", width: 40, height: 19, maxSpeed: 168, reverseSpeed: 58, acceleration: 86, reverseAcceleration: 62, brake: 184, drag: 44, steerRate: 1.82, maxHealth: 118, trunkCapacity: 7, cameraZoomFactor: 0.80, color: 0x6e5b37, trim: 0xffcf87 }),
-  police: Object.freeze({ id: "police", label: "Police cruiser", width: 35, height: 17, maxSpeed: 226, reverseSpeed: 76, acceleration: 132, reverseAcceleration: 82, brake: 224, drag: 50, steerRate: 2.42, maxHealth: 104, trunkCapacity: 3, cameraZoomFactor: 0.70, color: 0x294c7a, trim: 0x9dcaff })
+  compact: Object.freeze({ id: "compact", label: "Compact", width: 28, height: 14, maxSpeed: 245, reverseSpeed: 78, acceleration: 158, reverseAcceleration: 94, brake: 232, handbrakeBrake: 310, handbrakeSteerMultiplier: 1.72, drag: 58, steerRate: 2.62, maxHealth: 72, trunkCapacity: 2, cameraZoomFactor: 0.72, color: 0x78c7a3, trim: 0xd7ffec }),
+  sedan: Object.freeze({ id: "sedan", label: "Sedan", width: 34, height: 16, maxSpeed: 272, reverseSpeed: 84, acceleration: 146, reverseAcceleration: 90, brake: 222, handbrakeBrake: 300, handbrakeSteerMultiplier: 1.66, drag: 52, steerRate: 2.38, maxHealth: 88, trunkCapacity: 4, cameraZoomFactor: 0.68, color: 0x9a7ab8, trim: 0xefe6ff }),
+  van: Object.freeze({ id: "van", label: "Van", width: 40, height: 19, maxSpeed: 218, reverseSpeed: 70, acceleration: 116, reverseAcceleration: 76, brake: 202, handbrakeBrake: 278, handbrakeSteerMultiplier: 1.52, drag: 48, steerRate: 1.92, maxHealth: 118, trunkCapacity: 7, cameraZoomFactor: 0.75, color: 0x6e5b37, trim: 0xffcf87 }),
+  police: Object.freeze({ id: "police", label: "Police cruiser", width: 35, height: 17, maxSpeed: 292, reverseSpeed: 90, acceleration: 172, reverseAcceleration: 102, brake: 242, handbrakeBrake: 326, handbrakeSteerMultiplier: 1.70, drag: 54, steerRate: 2.48, maxHealth: 104, trunkCapacity: 3, cameraZoomFactor: 0.64, color: 0x294c7a, trim: 0x9dcaff })
 });
 
 export const vehicleDefinitions = Object.freeze([
