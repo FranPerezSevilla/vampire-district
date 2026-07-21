3cb7a92ebef2809d53bebf7f4a075d7dd5f4cab6
FranPerezSevilla <fjperezga@gmail.com>
2026-07-21 13:46:22 +0200

    Use Enter for vehicles and reserve Space for handbrake

--- phaser/src/runtime/GameplayRuntime.js
+++ phaser/src/runtime/GameplayRuntime.js
@@ -3,6 +3,12 @@ import { StreetFurnitureSystem } from "../systems/StreetFurnitureSystem.js";
 import { VehicleSystem } from "../vehicles/VehicleSystem.js";
 import { GameplayRuntime as GameplayRuntimeCore } from "./GameplayRuntimeCore.js";
 
+const VEHICLE_ACTION_TYPES = new Set(["vehicleEnter", "vehicleExit"]);
+
+function isVehicleAction(option) {
+  return VEHICLE_ACTION_TYPES.has(option?.type);
+}
+
 export class GameplayRuntime extends GameplayRuntimeCore {
   installDiagnostics() {
     super.installDiagnostics();
@@ -26,15 +32,51 @@ export class GameplayRuntime extends GameplayRuntimeCore {
     const scene = this.scene;
     const input = scene.inputSystem;
     const vehicle = scene.vehicleSystem;
-    const drivingAtFrameStart = Boolean(vehicle?.isDriving?.());
     const originalBeginFrame = input?.beginFrame;
+    const originalCollectInteractions = scene.collectInteractions;
     const dt = Math.min(Math.max(0, Number(deltaMs) || 0) / 1000, 0.05);
+
     scene.pedestrianSystem?.update?.(dt);
-    if (drivingAtFrameStart && input && originalBeginFrame) {
-      input.beginFrame = function vehicleInputFrame() { return vehicle.filterInputFrame(originalBeginFrame.call(input)); };
+
+    if (input && originalBeginFrame) {
+      input.beginFrame = function vehicleAwareInputFrame() {
+        const frame = originalBeginFrame.call(input);
+        const vehicleActionPressed = Boolean(frame.menuConfirmPressed && !frame.interactPressed);
+        const enriched = {
+          ...frame,
+          vehicleActionPressed,
+          handbrakeHeld: Boolean(input.keys?.space?.isDown)
+        };
+        if (vehicle?.isDriving?.()) return vehicle.filterInputFrame(enriched);
+        return vehicleActionPressed
+          ? { ...enriched, traversePressed: true }
+          : enriched;
+      };
     }
-    try { super.update(time, deltaMs); }
-    finally { if (input && originalBeginFrame) input.beginFrame = originalBeginFrame; }
+
+    if (typeof originalCollectInteractions === "function") {
+      scene.collectInteractions = function vehicleAwareInteractions() {
+        const options = originalCollectInteractions.call(scene) || [];
+        const frame = scene.currentInputFrame;
+        if (frame?.vehicleActionPressed) return options.filter(isVehicleAction);
+        if (frame?.traversePressed) return options.filter(option => !isVehicleAction(option));
+        return options;
+      };
+    }
+
+    try {
+      super.update(time, deltaMs);
+    } finally {
+      if (input && originalBeginFrame) input.beginFrame = originalBeginFrame;
+      if (originalCollectInteractions) scene.collectInteractions = originalCollectInteractions;
+    }
+  }
+
+  finishFrame() {
+    super.finishFrame();
+    if (isVehicleAction(this.scene.nearestMovement)) {
+      this.scene.traversalPromptLabel?.setText?.("ENTER");
+    }
   }
 
   destroy() {
