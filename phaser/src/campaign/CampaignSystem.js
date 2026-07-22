0f0cb092da6458394778ac230d5d2f3626c442d0
FranPerezSevilla <fjperezga@gmail.com>
2026-07-22 22:53:10 +0200

    Attach vehicle maintenance to campaign services

--- phaser/src/campaign/CampaignSystem.js
+++ phaser/src/campaign/CampaignSystem.js
@@ -1,5 +1,6 @@
 import { vehicleDefinitions } from "../data/vehicles.js";
 import { CampaignVehicleSystem } from "../vehicles/CampaignVehicleSystem.js";
+import { VehicleMaintenanceService } from "../vehicles/VehicleMaintenanceService.js";
 import {
   CampaignSystem as CampaignSystemCore,
   DEFAULT_DEFINITIONS
@@ -8,21 +9,31 @@ import {
 export class CampaignSystem extends CampaignSystemCore {
   buildServices() {
     super.buildServices();
+    const dirty = () => {
+      this.touch();
+      if (this.autoSave) this.save();
+    };
     this.vehicles = new CampaignVehicleSystem(this.state, {
       events: this.events,
       now: this.now,
-      onDirty: () => {
-        this.touch();
-        if (this.autoSave) this.save();
-      }
+      onDirty: dirty
     });
     this.vehicles.ensureStartingOwnership(vehicleDefinitions);
+    this.vehicleMaintenance = new VehicleMaintenanceService(this.state, {
+      wallet: this.wallet,
+      vehicles: this.vehicles,
+      definitions: vehicleDefinitions,
+      events: this.events,
+      now: this.now,
+      onDirty: dirty
+    });
   }
 
   snapshot() {
     return {
       ...super.snapshot(),
-      vehicles: this.vehicles.snapshot(vehicleDefinitions)
+      vehicles: this.vehicles.snapshot(vehicleDefinitions),
+      vehicleMaintenance: this.vehicleMaintenance.snapshot()
     };
   }
 
