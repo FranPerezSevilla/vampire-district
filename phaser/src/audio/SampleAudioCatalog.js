e343cb7f251e4f0e22c79fbe1b689e765ea618d6
FranPerezSevilla <fjperezga@gmail.com>
2026-08-15 06:06:10 +0200

    Tune civilian scream mix

--- phaser/src/audio/SampleAudioCatalog.js
+++ phaser/src/audio/SampleAudioCatalog.js
@@ -42,7 +42,7 @@ export const SAMPLE_AUDIO_CATALOG = Object.freeze({
     "phaser/assets/audio/civilians/civilian-scream-04.mp3",
     "phaser/assets/audio/civilians/civilian-scream-05.mp3",
     "phaser/assets/audio/civilians/civilian-scream-06.mp3"
-  ], { volume: 1.0 })
+  ], { volume: 0.82 })
 });
 
 export const SAMPLE_AUDIO_IDS = Object.freeze(Object.keys(SAMPLE_AUDIO_CATALOG));
