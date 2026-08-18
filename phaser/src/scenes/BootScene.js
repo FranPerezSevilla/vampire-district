23f1f4f5fca97cf8689d37a66fc1496d33776243
FranPerezSevilla <fjperezga@gmail.com>
2026-08-18 08:48:33 +0200

    feat(menu): boot into ViceBlood main menu

--- phaser/src/scenes/BootScene.js
+++ phaser/src/scenes/BootScene.js
@@ -4,11 +4,10 @@ export class BootScene extends Phaser.Scene {
   }
 
   create() {
-    this.registry.set("buildName", "Phaser migration Phase 0");
+    this.registry.set("buildName", "ViceBlood main menu V1");
     this.registry.set("currentLayer", 2);
     this.registry.set("statusText", "Rooftop refuge · movement sandbox");
 
-    this.scene.start("GameScene");
-    this.scene.launch("UIScene");
+    this.scene.start("MainMenuScene");
   }
 }
